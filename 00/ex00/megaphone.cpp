92867c19d173b176726c20905a435ec52f38d093
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-08-10 12:34:31 +0900

    algorithmを使わないように修正

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,40 +6,46 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 12:04:40 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/08/10 11:16:52 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/08/10 12:32:55 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 /**
  * @brief Output strs in uppercase to the console.
- * 
+ *
  * If no specification is given, the default character is output.
- * 
- * @param strs 
+ *
+ * @param strs
  */
 static void	local_megaphone(char **strs)
 {
+	int				strs_i;
+	int 			str_i;
+	unsigned char	uc;
+
 	if (strs == NULL)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 		return ;
 	}
-	int i = 1;
-	while (strs[i])
+	strs_i = 1;
+	while (strs[strs_i])
 	{
-		std::string s = strs[i++];
-		std::transform(s.begin(), s.end(), s.begin(),
-			static_cast<int(*)(int)>(std::toupper));
-		std::cout << s;
+		str_i = 0;
+		while(strs[strs_i][str_i] != '\0')
+		{
+			uc = std::toupper(strs[strs_i][str_i++]);
+			std::cout << uc;
+		}
+		strs_i++;
 	}
 	std::cout << "\n";
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	if (ac == 1)
 		local_megaphone(NULL);
