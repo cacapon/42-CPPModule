19b04764aee2861bac19c60aa79fc5275025e4f2
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-08-15 10:50:50 +0900

    枠組みだけ作成

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -6,7 +6,65 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 12:06:21 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:06:22 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/08/15 10:50:38 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <Account.hpp>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts()	{ return _nbAccounts; }
+int Account::getTotalAmount()	{ return _totalAmount; }
+int Account::getNbDeposits()	{ return _totalNbDeposits; }
+int	Account::getNbWithdrawals()	{ return _totalNbWithdrawals; }
+
+/**
+ * @brief display,acounts,total, deposits and withdrawals.
+ * 
+ */
+void	Account::displayAccountsInfos()
+{
+	//TODO
+}
+
+Account::Account(int initial_deposit)
+{
+	_nbDeposits = initial_deposit;
+}
+Account::~Account() 
+{
+	//TODO
+}
+
+void Account::makeDeposit(int deposit)
+{
+	//TODO
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	//TODO
+}
+
+int Account::checkAmount() const
+{
+	return (_amount);
+}
+
+void Account::displayStatus() const
+{
+	//TODO
+}
+
+/* private */
+
+void Account::_displayTimestamp()
+{
+	//TODO
+}
+Account::Account()
+{}
