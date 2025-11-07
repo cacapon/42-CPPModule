d0a32d88c191cd113623db683ac44d18a437cf7f
Cacapon Project <cacapon.project@gmail.com>
2025-11-07 01:46:18 +0000

    add 01/ex05

--- 01/ex05/main.cpp
+++ 01/ex05/main.cpp
@@ -1,12 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:09:12 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:09:13 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file main.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief That bacon Cramer has come.
+ * 
+ * 
+ */
 
+#include "Harl.hpp"
+#include <iostream>
+
+int main(void)
+{
+    Harl*   harl = new Harl();
+
+    std::cout << std::endl;
+    harl->complain("DEBUG");
+    std::cout << std::endl;
+    harl->complain("INFO");
+    std::cout << std::endl;
+    harl->complain("WARNING");
+    std::cout << std::endl;
+    harl->complain("ERROR");
+    std::cout << std::endl;
+    harl->complain("ELSE");
+    std::cout << std::endl;
+    delete harl;
+    return (0);
+}
