c8e6968a0d7c53fa13b7a1278112c584a50dad30
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-11-08 14:08:51 +0900

    add 01/ex06

--- 01/ex06/Harl.cpp
+++ 01/ex06/Harl.cpp
@@ -1,12 +1,82 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:14:56 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:14:58 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file Harl.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief Bacon-loving complainer
+ * 
+ * 
+ */
 
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+    std::cout << "Since he's here, we'll need to keep an eye on the bacon supply." << std::endl;
+}
+Harl::~Harl(void)
+{
+    std::cout << "He's gone. He ate all the bacon in the store." << std::endl;
+}
+
+
+void    Harl::complain(std::string level)
+{
+    switch (getCmd(level))
+    {
+    case 0:
+        debug();
+        break;
+    case 1:
+        info();
+        break;
+    case 2:
+        warning();
+        break;
+    case 3:
+        error();
+        break;
+    default:
+        default_msg();
+        break;
+    }
+}
+
+int   Harl::getCmd(std::string level)
+{
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i=0; i<4; i++)
+        if (levels[i] == level)
+            return (i);
+    return (-1);
+}
+
+void    Harl::debug(void)
+{
+    std::cout 
+        << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl
+        << "I really do!" << std::endl;
+}
+
+void    Harl::info(void)
+{
+    std::cout
+        << "I cannot believe adding extra bacon costs more money." << std::endl
+        << "You didn’t put enough bacon in my burger!" << std::endl
+        << "If you did, I wouldn’t be asking for more!" << std::endl;
+}
+void    Harl::warning(void)
+{
+    std::cout 
+        << "I think I deserve to have some extra bacon for free." << std::endl
+        << "I’ve been coming for years, whereas you started working here just last month."  << std::endl;
+}
+void    Harl::error(void)
+{
+    std::cout 
+        << "This is unacceptable!"  << std::endl
+        << "I want to speak to the manager now."  << std::endl;
+}
+void    Harl::default_msg(void)
+{
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
--- 01/ex06/Harl.hpp
+++ 01/ex06/Harl.hpp
@@ -1,12 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:14:59 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:15:03 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file Harl.hpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief Bacon-loving complainer
+ * 
+ * 
+ */
 
+#ifndef HARL_HPP
+# define HARL_HPP
+# include <string>
+# include <iostream>
+
+class Harl
+{
+    public:
+        Harl(void);
+        ~Harl(void);
+        void complain(std::string level);
+        int     getCmd(const std::string level);
+    private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+        void    default_msg(void);
+};
+
+#endif
