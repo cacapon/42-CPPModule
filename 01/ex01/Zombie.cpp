c658647c56bbb8b180d6238bd3ee9a6770c08c81
Cacapon Project <cacapon.project@gmail.com>
2025-11-14 02:28:45 +0000

    formatting 01/ex01

--- 01/ex01/Zombie.cpp
+++ 01/ex01/Zombie.cpp
@@ -1,28 +1,22 @@
 /**
  * @file Zombie.cpp
  * @author ttsubo (ttsubo@student.42.fr)
- * @brief 
- * 
- * 
+ * @brief
+ *
+ *
  */
 
-#include <iostream>
 #include "Zombie.hpp"
+#include <iostream>
 
+Zombie::Zombie(void) {}
 
-Zombie::Zombie(void){}
-
-Zombie::~Zombie(void)
-{
-    std::cout << this->name << " has returned to the earth\n";
+Zombie::~Zombie(void) {
+  std::cout << this->name << " has returned to the earth\n";
 }
 
-void Zombie::setName(std::string name)
-{
-    this->name = name;
-}
+void Zombie::setName(std::string name) { this->name = name; }
 
-void Zombie::announce(void)
-{
-	std::cout << this->name << ":BraiiiiiiinnnzzzZ...\n";
+void Zombie::announce(void) {
+  std::cout << this->name << ":BraiiiiiiinnnzzzZ...\n";
 }
