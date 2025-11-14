27184e0dfeb6e66a3aa2eae2956ad6005a600d28
Cacapon Project <cacapon.project@gmail.com>
2025-11-14 02:33:26 +0000

    formatting 01/ex05

--- 01/ex05/Harl.hpp
+++ 01/ex05/Harl.hpp
@@ -2,28 +2,28 @@
  * @file Harl.hpp
  * @author ttsubo (ttsubo@student.42.fr)
  * @brief Bacon-loving complainer
- * 
- * 
+ *
+ *
  */
 
 #ifndef HARL_HPP
-# define HARL_HPP
-# include <string>
-# include <iostream>
+#define HARL_HPP
+#include <iostream>
+#include <string>
 
-class Harl
-{
-    public:
-        Harl(void);
-        ~Harl(void);
-        void complain(std::string level);
-    private:
-        int     getCmd(const std::string level);
-        void    debug(void);
-        void    info(void);
-        void    warning(void);
-        void    error(void);
-        void    default_msg(void);
+class Harl {
+public:
+  Harl(void);
+  ~Harl(void);
+  void complain(std::string level);
+
+private:
+  int getCmd(const std::string level);
+  void debug(void);
+  void info(void);
+  void warning(void);
+  void error(void);
+  void default_msg(void);
 };
 
 #endif
--- 01/ex05/main.cpp
+++ 01/ex05/main.cpp
@@ -2,28 +2,27 @@
  * @file main.cpp
  * @author ttsubo (ttsubo@student.42.fr)
  * @brief That bacon Cramer has come.
- * 
- * 
+ *
+ *
  */
 
 #include "Harl.hpp"
 #include <iostream>
 
-int main(void)
-{
-    Harl*   harl = new Harl();
+int main(void) {
+  Harl *harl = new Harl();
 
-    std::cout << std::endl;
-    harl->complain("DEBUG");
-    std::cout << std::endl;
-    harl->complain("INFO");
-    std::cout << std::endl;
-    harl->complain("WARNING");
-    std::cout << std::endl;
-    harl->complain("ERROR");
-    std::cout << std::endl;
-    harl->complain("ELSE");
-    std::cout << std::endl;
-    delete harl;
-    return (0);
+  std::cout << std::endl;
+  harl->complain("DEBUG");
+  std::cout << std::endl;
+  harl->complain("INFO");
+  std::cout << std::endl;
+  harl->complain("WARNING");
+  std::cout << std::endl;
+  harl->complain("ERROR");
+  std::cout << std::endl;
+  harl->complain("ELSE");
+  std::cout << std::endl;
+  delete harl;
+  return (0);
 }
