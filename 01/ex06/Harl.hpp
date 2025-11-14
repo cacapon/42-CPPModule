4c84db3a1991ad300f3b6f75508d6de7dbb28f71
Cacapon Project <cacapon.project@gmail.com>
2025-11-14 02:36:02 +0000

    formatting 01/ex06

--- 01/ex06/Harl.hpp
+++ 01/ex06/Harl.hpp
@@ -2,28 +2,28 @@
  * @file Harl.hpp
  * @author ttsubo (ttsubo@student.42.fr)
  * @brief Bacon-loving complainer
- * 
- * 
+ *
+ *
  */
 
 #ifndef HARL_HPP
-# define HARL_HPP
-# include <string>
-# include <iostream>
+#define HARL_HPP
+#include <iostream>
+#include <string>
 
-class Harl
-{
-    public:
-        Harl(void);
-        ~Harl(void);
-        void complain(std::string level);
-        int     getCmd(const std::string level);
-    private:
-        void    debug(void);
-        void    info(void);
-        void    warning(void);
-        void    error(void);
-        void    default_msg(void);
+class Harl {
+public:
+  Harl(void);
+  ~Harl(void);
+  void complain(std::string level);
+  int getCmd(const std::string level);
+
+private:
+  void debug(void);
+  void info(void);
+  void warning(void);
+  void error(void);
+  void default_msg(void);
 };
 
 #endif
--- 01/ex06/main.cpp
+++ 01/ex06/main.cpp
@@ -1,39 +1,35 @@
 /**
  * @file main.cpp
  * @author ttsubo (ttsubo@student.42.fr)
- * @brief 
- * 
+ * @brief
+ *
  */
 
 #include "Harl.hpp"
 #include <iostream>
 
-void	usage(void)
-{
-	std::cout << "Usage: ./harlFilter <LOG Level>" << std::endl;
+void usage(void) {
+  std::cout << "Usage: ./harlFilter <LOG Level>" << std::endl;
 }
 
-int	main(int ac, char **av)
-{
-	if (ac != 2)
-		return (usage(), 1);
+int main(int ac, char **av) {
+  if (ac != 2)
+    return (usage(), 1);
 
-	Harl*   harl = new Harl();
-	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	int loglevel = harl->getCmd(av[1]);
+  Harl *harl = new Harl();
+  std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+  int loglevel = harl->getCmd(av[1]);
 
-	if (loglevel == -1)
-		harl->complain("default");
-	else
-	{
-		for (int i=loglevel; i<4; i++)
-		{
-			std::cout << "[ " << levels[i] << " ]" << std::endl;
-			harl->complain(levels[i]);
-			std::cout << std::endl;
-		}
-	}
+  if (loglevel == -1)
+    harl->complain("default");
+  else {
+    for (int i = loglevel; i < 4; i++) {
+      std::cout << "[ " << levels[i] << " ]" << std::endl;
+      harl->complain(levels[i]);
+      std::cout << std::endl;
+    }
+  }
 
-	delete harl;
-    return (0);
+  delete harl;
+  return (0);
 }
