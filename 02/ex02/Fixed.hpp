180135a2121c423cb5b4b79650c05788cdf0005a
Cacapon Project <cacapon.project@gmail.com>
2025-11-14 02:12:31 +0000

    add 02/ex02

--- 02/ex02/Fixed.hpp
+++ 02/ex02/Fixed.hpp
@@ -1,12 +1,61 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:19:55 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:19:57 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file Fixed.hpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief 
+ * 
+ * 
+ */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+
+class Fixed
+{
+    public:
+        Fixed(void);
+        Fixed(const Fixed& other);
+        Fixed(const int i);
+        Fixed(const float f);
+        Fixed& operator=(const Fixed& other);
+        ~Fixed();
+
+        // comparison operators
+        bool operator>(const Fixed& other);
+        bool operator<(const Fixed& other);
+        bool operator>=(const Fixed& other);
+        bool operator<=(const Fixed& other);
+        bool operator==(const Fixed& other);
+        bool operator!=(const Fixed& other);
+
+        // arithmetic operators
+        Fixed operator+(const Fixed& other) const;
+        Fixed operator-(const Fixed& other) const;
+        Fixed operator*(const Fixed& other) const;
+        Fixed operator/(const Fixed& other) const;
+
+        // increment & decrement operators
+        Fixed& operator++();
+        Fixed operator++(int);
+        Fixed& operator--();
+        Fixed operator--(int);
+
+        // static member functions
+        static Fixed& max(Fixed& a, Fixed& b);
+        static const Fixed& max(const Fixed& a, const Fixed& b);
+        static Fixed& min(Fixed& a, Fixed& b);
+        static const Fixed& min(const Fixed& a, const Fixed& b);
+
+        // menber functions
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        float   toFloat(void) const;
+        int     toInt(void) const;
+    private:
+        int val;
+        static const int bits = 8;
+};
+
+std::ostream    &operator<<(std::ostream &o, const Fixed &fixed);
+
+#endif
--- 02/ex02/main.cpp
+++ 02/ex02/main.cpp
@@ -1,12 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:19:58 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:19:59 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file main.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief 
+ * 
+ * 
+ */
 
+#include <iostream>
+#include "Fixed.hpp"
+
+int main(void) {
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max(a, b) << std::endl;
+
+    return 0;
+}
