a646b78f7b704fef9cf83fdd396115dc91517d69
Cacapon Project <cacapon.project@gmail.com>
2025-11-18 06:06:41 +0000

    add 03/ex00

--- 03/ex00/ClapTrap.hpp
+++ 03/ex00/ClapTrap.hpp
@@ -1,12 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:32:39 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:32:41 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file ClapTrap.hpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief To me, it looks like nothing but a crocodile.
+ *
+ *
+ */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+class ClapTrap {
+  public:
+    ClapTrap();
+    ClapTrap(const std::string &name);
+    ClapTrap(const ClapTrap &other);
+    ClapTrap &operator=(const ClapTrap &other);
+    ~ClapTrap();
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+    std::string getName() const;
+    unsigned int getHp() const;
+    unsigned int getEp() const;
+    unsigned int getAtk() const;
+
+  private:
+    std::string name;
+    unsigned int hp;
+    unsigned int ep;
+    unsigned int atk;
+    void showName();
+};
+
+#endif
--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -1,12 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:32:41 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:32:43 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file main.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief
+ *
+ *
+ */
 
+#include "ClapTrap.hpp"
+int main(void) {
+    {
+        // normal test
+        ClapTrap *c1 = new ClapTrap("Normal");
+        c1->attack("Trap1");
+        c1->takeDamage(1);
+        c1->beRepaired(1);
+        delete c1;
+    }
+    {
+        // ep test
+        ClapTrap *c2 = new ClapTrap("EPTest");
+        for (int i = 0; i < 5; i++) {
+            c2->attack("Trap1");
+            c2->beRepaired(1);
+        }
+        c2->attack("Attack Failed");
+        c2->beRepaired(1);
+        c2->takeDamage(1);
+        delete c2;
+    }
+    {
+        // hp test
+        ClapTrap *c3 = new ClapTrap("HPTest");
+        c3->takeDamage(10);
+        c3->attack("Attack Failed");
+        c3->beRepaired(1);
+        c3->takeDamage(1);
+        delete c3;
+    }
+    return 0;
+}
