697f61a20e62c9cff5e9fcb371d7cc54ecb2c6d7
Cacapon Project <cacapon.project@gmail.com>
2025-11-18 07:34:20 +0000

    add 03/ex01

--- 03/ex01/ScavTrap.hpp
+++ 03/ex01/ScavTrap.hpp
@@ -6,7 +6,25 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 12:32:57 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:32:59 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/11/18 07:02:12 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(const std::string &name);
+        ScavTrap(const ScavTrap& other);
+        ScavTrap& operator=(const ScavTrap& other);
+        ~ScavTrap();
+        void guardGate();
+    private:
+        virtual void showName() const;      
+};
+
+#endif
--- 03/ex01/main.cpp
+++ 03/ex01/main.cpp
@@ -1,12 +1,82 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:32:51 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:32:52 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file main.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief
+ *
+ *
+ */
 
+#include "ScavTrap.hpp"
+#include <iostream>
+#include <ostream>
+
+int main(void) {
+    {
+        std::cout << "<ctor/dtor test>" << std::endl;
+        ScavTrap *default_ctor = new ScavTrap();
+        ScavTrap *name_ctor = new ScavTrap("NameCtor");
+        ScavTrap *copy_ctor = new ScavTrap(*default_ctor);
+        delete default_ctor;
+        delete name_ctor;
+        delete copy_ctor;
+        std::cout << "================" << std::endl;
+    }
+    {
+        // normal test
+        std::cout << "<normal test>" << std::endl;
+        ScavTrap *normal = new ScavTrap("Normal");
+        normal->attack("Scav1");
+        normal->takeDamage(1);
+        normal->beRepaired(1);
+        normal->guardGate();
+        delete normal;
+        std::cout << "================" << std::endl;
+    }
+    {
+        std::cout << "<ep test1: Crap ep>" << std::endl;
+        ScavTrap *ep1 = new ScavTrap("EPTest1");
+        for (int i = 0; i < 5; i++) {
+            ep1->attack("Enemy");
+            ep1->beRepaired(1);
+        }
+        ep1->attack("Attack success");
+        ep1->beRepaired(1);
+        ep1->takeDamage(1);
+        delete ep1;
+        std::cout << "================" << std::endl;
+    }
+    {
+        std::cout << "<ep test2: Scav ep>" << std::endl;
+        ScavTrap *ep2 = new ScavTrap("EPTest2");
+        for (int i = 0; i < 25; i++) {
+            ep2->attack("Enemy");
+            ep2->beRepaired(1);
+        }
+        ep2->attack("Attack failed");
+        ep2->beRepaired(1);
+        ep2->takeDamage(1);
+        delete ep2;
+        std::cout << "================" << std::endl;
+    }
+    {
+        std::cout << "<hp test1: Clap hp>" << std::endl;
+        ScavTrap *hp1 = new ScavTrap("HPTest1");
+        hp1->takeDamage(10);
+        hp1->attack("Attack Success");
+        hp1->beRepaired(1);
+        hp1->takeDamage(1);
+        delete hp1;
+        std::cout << "================" << std::endl;
+    }
+    {
+        std::cout << "<hp test2: Scav hp>" << std::endl;
+        ScavTrap *hp2 = new ScavTrap("HPTest2");
+        hp2->takeDamage(100);
+        hp2->attack("Attack Failed");
+        hp2->beRepaired(1);
+        hp2->takeDamage(1);
+        delete hp2;
+        std::cout << "================" << std::endl;
+    }
+    return 0;
+}
