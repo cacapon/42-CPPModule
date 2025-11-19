a47d09503fcc8a0624ebd5ddb53169e5fa6f4233
Cacapon Project <cacapon.project@gmail.com>
2025-11-19 02:13:33 +0000

    fix ScavTrap

--- 03/ex01/ScavTrap.hpp
+++ 03/ex01/ScavTrap.hpp
@@ -6,25 +6,30 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 12:32:57 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/11/18 07:02:12 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/11/19 02:12:03 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
-# define SCAVTRAP_HPP
-# include "ClapTrap.hpp"
+#define SCAVTRAP_HPP
+#include "ClapTrap.hpp"
 
-class ScavTrap : public ClapTrap
-{
-    public:
-        ScavTrap();
-        ScavTrap(const std::string &name);
-        ScavTrap(const ScavTrap& other);
-        ScavTrap& operator=(const ScavTrap& other);
-        ~ScavTrap();
-        void guardGate();
-    private:
-        virtual void showName() const;      
+class ScavTrap : public ClapTrap {
+  public:
+    ScavTrap();
+    ScavTrap(const std::string &name);
+    ScavTrap(const ScavTrap &other);
+    ScavTrap &operator=(const ScavTrap &other);
+    ~ScavTrap();
+    void guardGate();
+
+  protected:
+    static const unsigned int HP = 100;
+    static const unsigned int EP = 50;
+    static const unsigned int ATK = 20;
+
+  private:
+    virtual void showName() const;
 };
 
 #endif
