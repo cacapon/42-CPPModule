1022c5bb51c622c8aa62f4e0e661f8df593b933e
Cacapon Project <cacapon.project@gmail.com>
2025-11-25 08:23:58 +0000

    add 04/ex01

--- 04/ex01/main.cpp
+++ 04/ex01/main.cpp
@@ -1,12 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:35:11 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:35:12 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file main.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief
+ *
+ *
+ */
 
+#include <iostream>
+
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+int main() {
+    const int MAX = 10;
+    Animal *animals[MAX];
+    std::cout << "<<<instantiate>>>" << std::endl;
+    for (int i = 0; i < MAX; i += 2) {
+        animals[i] = new Dog();
+        animals[i + 1] = new Cat();
+    }
+    std::cout << "<<<makeSound>>>" << std::endl;
+    for (int i = 0; i < MAX; i++) {
+        std::cout << animals[i]->getType() << ":";
+        animals[i]->makeSound();
+    }
+    std::cout << "<<<delete>>>" << std::endl;
+    for (int i = 0; i < MAX; i++) delete animals[i];
+    return 0;
+}
