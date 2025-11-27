a6af34850a01ed40976fb0b1c73d54d2a0063fd4
Cacapon Project <cacapon.project@gmail.com>
2025-11-27 05:14:35 +0000

    ex03: Add AMateria

---
+++ 04/ex03/AMateria.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file AMateria.hpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief
+ *
+ *
+ */
+
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <string>
+
+#include "ICharacter.hpp"
+class AMateria {
+   public:
+    AMateria();
+    AMateria(std::string const& type);
+    AMateria(const AMateria& other);
+    AMateria& operator=(const AMateria& other);
+    virtual ~AMateria();
+    std::string const& getType() const;
+    virtual AMateria* clone() const = 0;
+    virtual void use(ICharacter& target);
+
+   protected:
+    std::string type;
+
+   private:
+};
+
+#endif
