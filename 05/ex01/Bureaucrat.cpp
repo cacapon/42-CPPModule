a404f9928952cd084b45267680024bc8a96bbb69
Cacapon Project <cacapon.project@gmail.com>
2025-12-02 09:20:29 +0000

    add ex01

--- 05/ex01/Bureaucrat.cpp
+++ 05/ex01/Bureaucrat.cpp
@@ -1,12 +1,81 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:39:17 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:39:19 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file Bureaucrat.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief
+ *
+ *
+ */
 
+#include "Bureaucrat.hpp"
+
+#include <iostream>
+#include <string>
+
+#include "Form.hpp"
+
+static void ShowLog(const std::string detail) {
+#ifdef DEBUG
+    std::cout << "Bureaucrat::" << detail << std::endl;
+#else
+    (void)detail;
+#endif
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+    : name(name), grade(grade) {
+    if (grade < 1) throw GradeTooHighException();
+    if (grade > 150) throw GradeTooLowException();
+    ShowLog("name ctor");
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+    : name(other.name), grade(other.grade) {
+    ShowLog("copy ctor");
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+    if (this != &other) {
+        this->grade = other.grade;
+    }
+    ShowLog("operator=");
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() { ShowLog("dtor"); }
+
+void Bureaucrat::promote() {
+    if (grade <= 1) throw GradeTooHighException();
+    this->grade--;
+}
+
+void Bureaucrat::demote() {
+    if (grade >= 150) throw GradeTooLowException();
+    this->grade++;
+}
+
+void Bureaucrat::signForm(Form& f) {
+    try {
+        f.beSigned(*this);
+        std::cout << this->getName() << " signed " << f.getName() << std::endl;
+    } catch (std::exception& e) {
+        std::cout << this->getName() << " couldn't sign " << f.getName()
+                  << " becouse " << e.what() << std::endl;
+    }
+}
+
+const std::string& Bureaucrat::getName() const { return this->name; }
+int Bureaucrat::getGrade() const { return this->grade; }
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return "Bureaucrat::GradeTooHighException";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Bureaucrat::GradeTooLowException";
+}
+
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& bureaucrat) {
+    o << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade()
+      << ".";
+    return (o);
+}
--- 05/ex01/Form.cpp
+++ 05/ex01/Form.cpp
@@ -1,12 +1,74 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Form.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 12:39:22 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/07/13 12:39:23 by ttsubo           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @file Form.cpp
+ * @author ttsubo (ttsubo@student.42.fr)
+ * @brief
+ *
+ *
+ */
 
+#include "Form.hpp"
+
+#include "Bureaucrat.hpp"
+
+static void ShowLog(const std::string detail) {
+#ifdef DEBUG
+    std::cout << "Form::" << detail << std::endl;
+#else
+    (void)detail;
+#endif
+}
+
+Form::Form(const std::string& name, const int signed_grade,
+           const int exec_grade)
+    : name(name), signed_grade(signed_grade), exec_grade(exec_grade) {
+    if (exec_grade < 1 || signed_grade < 1) throw GradeTooHighException();
+    if (exec_grade > 150 || signed_grade > 150) throw GradeTooLowException();
+    this->is_signed = false;
+    ShowLog("default ctor");
+}
+
+Form::Form(const Form& other)
+    : name(other.name),
+      signed_grade(other.signed_grade),
+      exec_grade(other.exec_grade),
+      is_signed(other.is_signed) {
+    ShowLog("copy ctor");
+}
+
+Form& Form::operator=(const Form& other) {
+    if (this != &other) {
+        this->is_signed = other.is_signed;
+    }
+    ShowLog("operator=");
+    return *this;
+}
+
+Form::~Form() { ShowLog("dtor"); }
+
+void Form::beSigned(Bureaucrat& b) {
+    if (b.getGrade() <= this->getSignedGrade())
+        this->is_signed = true;
+    else
+        throw Form::GradeTooLowException();
+}
+
+const std::string& Form::getName() const { return this->name; }
+bool Form::getIsSigned() const { return this->is_signed; }
+int Form::getSignedGrade() const { return this->signed_grade; }
+int Form::getExecGrade() const { return this->exec_grade; }
+
+const char* Form::GradeTooHighException::what() const throw() {
+    return "Form::GradeTooHighException";
+}
+
+const char* Form::GradeTooLowException::what() const throw() {
+    return "Form::GradeTooLowException";
+}
+
+std::ostream& operator<<(std::ostream& o, const Form& form) {
+    o << "Form(name=" << form.getName()
+      << ", signed_grade=" << form.getSignedGrade()
+      << ", exec_grade=" << form.getExecGrade()
+      << ", is_signed=" << form.getIsSigned() << ")";
+    return (o);
+}
