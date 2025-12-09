c651459383e21072366207f0a3b246f6e7d282ab
Cacapon Project <cacapon.project@gmail.com>
2025-12-09 00:53:15 +0000

    fix format

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -22,13 +22,14 @@ static void ShowLog(const std::string detail) {
 #endif
 }
 
-void ScalarConverter::convert(const std::string &str) {
-    fromChar(str);
-    fromInt(str);
-    fromFloat(str);
-    fromDouble(str);
+void ScalarConverter::convert(const std::string &s) {
+    fromChar(s);
+    fromInt(s);
+    fromFloat(s);
+    fromDouble(s);
 }
 
+/* canonical form - private because instantiation is not allowed */
 ScalarConverter::ScalarConverter() { ShowLog("Err:default ctor"); }
 
 ScalarConverter::ScalarConverter(const ScalarConverter &other) {
@@ -44,71 +45,139 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {
 
 ScalarConverter::~ScalarConverter() { ShowLog("Err:dtor"); }
 
-bool ScalarConverter::isChar(const std::string &str) {
-    if (str.length() != 1) return false;
-    unsigned char c = static_cast<unsigned char>(str[0]);
+/* Check whether the provided string can be converted to the type */
+bool ScalarConverter::isChar(const std::string &s) {
+    if (s.length() != 1) return false;
+    unsigned char c = static_cast<unsigned char>(s[0]);
     if (c > 127) return false;
     return true;
 }
 
-bool ScalarConverter::isInt(const std::string &str) {
+bool ScalarConverter::isInt(const std::string &s) {
+    if (regexInt(s))
+        return fitsInInt(s);
+    else if (regexFloat(s) || regexDouble(s)) {
+        if (!decimalIsZero(s)) return false;
+        return fitsInInt(extractIntegerPart(s));
+    } else
+        return false;
+}
+
+bool ScalarConverter::isFloat(const std::string &s) {
+    (void)s;
+    return false;
+}
+bool ScalarConverter::isDouble(const std::string &s) {
+    (void)s;
+    return false;
+}
+
+/* helper methods */
+// pre: The string s that is passed is known to already consist of integers.
+bool ScalarConverter::fitsInInt(const std::string &s) {
     size_t i = 0;
-    int digitCount;
-
-    if (str.empty()) return false;
-    if (str.length() == 1 && !std::isdigit(str[i])) return false;
-    if (str[i] == '+' || str[i] == '-') i++;
-
-    for (; i < str.length(); i++) {
-        if (!std::isdigit(str[i])) return false;
-    }
-    digitCount =
-        (str[0] == '+' || str[0] == '-') ? (str.length() - 1) : str.length();
+    if (s[0] == '+' || s[0] == '-') i++;
+    while (i < s.length() && s[i] == '0') i++;
+    int digitCount = s.length() - i;
     if (digitCount > 10) return false;
     if (digitCount == 10) {
-        if (str[0] == '-' && str.compare("-2147483648") > 0)
+        const std::string digits = s.substr(i);
+        if (s[0] == '-' && digits.compare("2147483648") > 0)
             return false;
-        else if (str.compare("+2147483648") > 0 &&
-                 str.compare("2147483648") > 0)
+        else if (s[0] != '-' && digits.compare("2147483647") > 0)
             return false;
     }
     return true;
 }
 
-bool ScalarConverter::isFloat(const std::string &str) {
-    (void)str;
-    return false;
-}
-bool ScalarConverter::isDouble(const std::string &str) {
-    (void)str;
-    return false;
+// ^[+-]?[0-9]+$
+bool ScalarConverter::regexInt(const std::string &s) {
+    size_t i = 0;
+    if (s.empty()) return false;
+    if (s[i] == '+' || s[i] == '-') i++;
+    size_t start = i;
+    while (i < s.length() && std::isdigit(s[i])) i++;
+    if (start == i) return false;
+    return i == s.length();
 }
 
-void ScalarConverter::fromChar(const std::string str) {
+// ^[+-]?[0-9]+\.[0-9]+f$
+bool ScalarConverter::regexFloat(const std::string &s) {
+    size_t i = 0;
+    if (s.empty()) return false;
+    if (s[i] == '+' || s[i] == '-') i++;
+    size_t startInt = i;
+    while (i < s.length() && std::isdigit(s[i])) i++;
+    if (i == startInt) return false;
+
+    if (i >= s.length() || s[i] != '.') return false;
+    i++;
+    size_t startFrac = i;
+    while (i < s.length() && std::isdigit(s[i])) i++;
+    if (startFrac == i) return false;
+
+    if (i != s.length() - 1) return false;
+    if (s[s.length() - 1] != 'f') return false;
+    return true;
+}
+
+// ^[+-]?[0-9]+(\.[0-9]+)?$
+bool ScalarConverter::regexDouble(const std::string &s) {
+    size_t i = 0;
+    if (s.empty()) return false;
+    if (s[i] == '+' || s[i] == '-') i++;
+    while (i < s.length() && std::isdigit(s[i])) i++;
+    if (i < s.length() && s[i] == '.') {
+        i++;
+        size_t startFrac = i;
+        while (i < s.length() && std::isdigit(s[i])) i++;
+        if (i == startFrac) return false;
+    }
+    return i == s.length();
+}
+
+// Check whether the digit after the decimal point is zero.
+bool ScalarConverter::decimalIsZero(const std::string &s) {
+    size_t pos = s.find('.');
+    if (pos == std::string::npos) return true;
+    pos++;
+    while (pos < s.length() && s[pos] == '0') pos++;
+    size_t lastDigit = (s[s.length() - 1] == 'f') ? s.length() - 1 : s.length();
+    return pos == lastDigit;
+}
+
+std::string ScalarConverter::extractIntegerPart(const std::string &s) {
+    size_t pos = s.find('.');
+    if (pos == std::string::npos) return s;
+    return s.substr(0, pos);
+}
+
+/* Output in that format. If not possible, return impossible. */
+void ScalarConverter::fromChar(const std::string &s) {
     std::cout << "char: ";
-    if (!isChar(str))
+    if (!isChar(s))
         std::cout << "impossible";
-    else if (!std::isprint(str[0]))
+    else if (!std::isprint(s[0]))
         std::cout << "Non displayable";
     else
-        std::cout << "'" << str[0] << "'";
+        std::cout << "'" << s[0] << "'";
     std::cout << std::endl;
 }
 
-void ScalarConverter::fromInt(const std::string str) {
+void ScalarConverter::fromInt(const std::string &s) {
     // TODO
     std::cout << "int: ";
-    if (!isInt(str))
+    if (!isInt(s))
         std::cout << "impossible";
     else
-        std::cout << std::atoi(str.c_str());
+        std::cout << std::atoi(s.c_str());
     std::cout << std::endl;
 }
-void ScalarConverter::fromFloat(const std::string str) {
+void ScalarConverter::fromFloat(const std::string &s) {
     // TODO
-    (void)str;
+    (void)s;
 }
-void ScalarConverter::fromDouble(const std::string str) {
+void ScalarConverter::fromDouble(const std::string &s) {
     // TODO
-    (void)str;
+    (void)s;
 }
--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -12,23 +12,37 @@
 
 class ScalarConverter {
    public:
-    static void convert(const std::string &str);
+    static void convert(const std::string &s);
 
    private:
+    /* canonical form - private because instantiation is not allowed */
     ScalarConverter();
     ScalarConverter(const ScalarConverter &other);
     ScalarConverter &operator=(const ScalarConverter &other);
     ~ScalarConverter();
 
-    static bool isChar(const std::string &str);
-    static bool isInt(const std::string &str);
-    static bool isFloat(const std::string &str);
-    static bool isDouble(const std::string &str);
+   private:
+    /* Check whether the provided string can be converted to the type */
+    static bool isChar(const std::string &s);
+    static bool isInt(const std::string &s);
+    static bool isFloat(const std::string &s);
+    static bool isDouble(const std::string &s);
 
-    static void fromChar(const std::string str);
-    static void fromInt(const std::string str);
-    static void fromFloat(const std::string str);
-    static void fromDouble(const std::string str);
+   private:
+    /* helper methods */
+    static bool fitsInInt(const std::string &s);
+    static bool regexInt(const std::string &s);
+    static bool regexFloat(const std::string &s);
+    static bool regexDouble(const std::string &s);
+    static bool decimalIsZero(const std::string &s);
+    static std::string extractIntegerPart(const std::string &s);
+
+   private:
+    /* Output in that format. If not possible, return impossible. */
+    static void fromChar(const std::string &s);
+    static void fromInt(const std::string &s);
+    static void fromFloat(const std::string &s);
+    static void fromDouble(const std::string &s);
 };
 
 #endif
